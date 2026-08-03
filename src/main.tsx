df58857d8ee7f416342840b9cd13502d60d2c5a8
Theoxenos <theoxenos@pm.me>
2026-08-03 10:43:20 +0200

    feat: install bootstrap-icons

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,10 @@
 import {StrictMode} from 'react';
 import {createRoot} from 'react-dom/client';
-import 'bootstrap/dist/css/bootstrap.min.css';
 import {createBrowserRouter, RouterProvider} from "react-router-dom";
 import router from "./router.tsx";
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap-icons/font/bootstrap-icons.css";
 
 createRoot(document.getElementById('root')!).render(
     <StrictMode>

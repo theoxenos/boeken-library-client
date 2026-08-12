f94d7568d43d1c5157b0dcbccbde23546bd9bfff
Theoxenos <theoxenos@pm.me>
2026-08-12 13:56:41 +0200

    refactor: indent Await component for better readability

--- src/features/books/BooksView.tsx
+++ src/features/books/BooksView.tsx
@@ -27,10 +27,11 @@ const BooksView = () => {
     return (
         <Row xs={1} md={4} lg={6} className="g-3">
             <Suspense fallback={<div>Loading...</div>}>
-                <Await key={booksPromise} resolve={booksPromise}>
+                <Await resolve={booksPromise}>
                     {(books: IBook[]) => books.map((book) => (
                         <Col key={book.id} className="mb-3 h-100">
-                            <BookItem book={book} onAddToLibrary={handleAddToLibrary}
+                            <BookItem book={book}
+                                      onAddToLibrary={handleAddToLibrary}
                                       onRemoveFromLibrary={handleRemoveFromLibrary}
                                       onSetRating={handleSetRating}/>
                         </Col>
